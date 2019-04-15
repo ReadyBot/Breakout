4520d1b2112dc11f00c0ff144c8cb1e789811a40
MrMokka <sartaren@hotmail.com>
2019-04-15 12:07:43 +0200

    Initial commit

--- SDL_Breakout/Collider.cpp
+++ SDL_Breakout/Collider.cpp
@@ -8,8 +8,10 @@ Collider::Collider(int x, int y, int w, int h) : x_(x), y_(y), w_(w), h_(h){
 Collider::~Collider(){}
 
 bool Collider::Colliding(Collider col1, Collider col2){
-	if(col1.x_ > col2.x_ && col1.y_ > col2.y_ &&
-		col1.x_ + col1.w_ < col2.x_ + col2.w_ && col1.y_ + col1.h_ < col2.y_ + col2.h_)
+	if(col1.x_ < col2.x_ + col2.w_ &&
+		col1.x_ + col1.w_ > col2.x_ &&
+		col1.y_ < col2.y_ + col2.h_ &&
+		col1.y_ + col1.h_ > col2.y_)
 		return true;
 	return false;
 }
